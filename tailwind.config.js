7926d3ac90cc0f6fdd8743d1998d703666a5f2f3
Natalia Denysenko <natalia.denysenko@mgid.com>
2022-08-04 11:13:26 +0300

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+  theme: {
+        colors: {
+            "initialColor": "rgb(255, 255, 255)",
+            "hoverColor": "rgb(0, 168, 244)",
+            "hoveredSquareBg": "#fbf8e3",
+            "hoveredSquareBorder": "#faeccd",
+        },
+    extend: {
+    },
+  },
+  plugins: [],
+}
